aa4662db09dba6086db27ee486a69cc3bd030628
Abhi Rao <abhirao@gmail.com>
2012-12-09 15:48:23 -0600

    Maxing max_eps a configurable value

--- app_config.rb
+++ app_config.rb
@@ -2,15 +2,11 @@ class AppConfig
   APP_CONFIG = YAML.load_file('config.yml') if File.exists? 'config.yml'
 
   def self.respond_to?(message)
-    [:dropbox_key, :dropbox_secret, :dropbox_session, :tapas_acct, :tapas_pwd].include? message
+    [:dropbox_key, :dropbox_secret, :dropbox_session, :tapas_acct, :tapas_pwd, :max_eps].include? message
   end
 
   def self.method_missing(message)
     return ENV[message.to_s] || APP_CONFIG[message.to_s] if self.respond_to? message
     super
-  end
-
-  def self.max_eps
-    1
   end
 end
